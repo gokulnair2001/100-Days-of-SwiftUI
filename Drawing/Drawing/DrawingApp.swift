6f1b818a60a92cc512b3c341d6b6c99f7247cd65
gokulnair2001 <gokulnair.2001@gmail.com>
2021-12-02 09:19:57 +0530

    Completed ⚡️

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Gokul Nair on 01/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            mainView()
+        }
+    }
+}
---
+++ Drawing/Drawing/Spectrum.swift
@@ -0,0 +1,66 @@
+//
+//  Spectrum.swift
+//  Drawing
+//
+//  Created by Gokul Nair on 01/12/21.
+//
+
+import SwiftUI
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) {value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        color(for: value, brightness: 1),
+                        color(for: value, brightness: 0.5)]),
+                        startPoint: .top,
+                        endPoint: .bottom),
+                        lineWidth: 2)
+            }
+        }.drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct Spectrum: View {
+    
+    @State private var colorCycle = 0.0
+    
+    
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            
+            VStack(alignment: .leading) {
+                Text("Cycle it")
+                    .font(.title)
+                    .bold()
+                Slider(value: $colorCycle)
+            }
+                .padding()
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct Spectrum_Previews: PreviewProvider {
+    static var previews: some View {
+        Spectrum()
+    }
+}
